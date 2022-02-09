0b606a20ca97c49b94bf6394e75c8772c0b31f03
Isaac_M <isaacmochon@gmail.com>
2022-02-08 21:15:35 -0600

    Se empezo el componente de login,hay que definir rutas para que sea la primer pantalla

--- src/pfsairqualityg8/src/App.js
+++ src/pfsairqualityg8/src/App.js
@@ -4,12 +4,14 @@ import Header from "./Components/Header/Header";
 // eslint-disable-next-line no-unused-vars
 import Form from "./Components/Form/Form";
 import HomeComponent from "./Components/Home/HomeComponent";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <>
       <Header />
       <HomeComponent />
+      <Login />
     </>
   );
 };
---
+++ src/pfsairqualityg8/src/Components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="login-component">
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
