dcf67f012604ed6cd1aa567695a93aa7f91ece15
Isaac_M <isaacmochon@gmail.com>
2022-02-09 21:18:59 -0600

    ultimos cambios

--- src/pfsairqualityg8/src/Components/CardHolder/CardHolder.jsx
+++ src/pfsairqualityg8/src/Components/CardHolder/CardHolder.jsx
@@ -3,11 +3,17 @@ import axios from "axios";
 import "./cardholder.css";
 
 const CardHolder = () => {
+  //pendiente crear estado para la info de la repsuesta
+
   const getData = () => {
     axios
       .get("https://rickandmortyapi.com/api/character/1,183")
       .then((response) => {
         console.log("response data", response.data);
+        for (let i = 0; i < response.data.length; i++) {
+          // eslint-disable-next-line no-unused-vars
+          let { image, description } = response.data[i];
+        }
       })
       .catch((error) => {
         console.log("error", error);
@@ -32,7 +38,7 @@ const CardHolder = () => {
       <div className="card-wrapper">
         <div className="card-body">
           <div>imagen</div>
-          <div>Descripcion</div>
+          <div>Descripcion{}</div>
         </div>
       </div>
     </section>
