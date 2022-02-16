e1e73fefce7b06dc61445a85526c4dc66f6e31be
carlos vasquez <carlosavrelsalvador@gmail.com>
2022-02-15 22:21:21 -0600

    fix mongoose

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,33 @@
-const Express = require('express');
-const Cors = require('cors');
+require("dotenv").config();
+
+const express = require('express');
+const mongoose = require("mongoose");
+const cors = require('cors');
+
+const config = require("./config");
+console.log("mongo_uri", config.mongo_uri);
+
+mongoose
+  .connect(config.mongo_uri)
+  .then(() => console.log("Mongo DB connected"))
+  .catch(() => console.log("Error in connection"));
 
 const PORT = process.env.PORT || 5000; // https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
-const app = Express();
+const app = express();
 const server = require('http').Server(app);
 
-app.use(Cors());
+app.use(cors());
 app.use('/health', (req, res) => {
   res.status(200).json({
-    appName: 'API cvasquez',
+    appName: 'API',
     version: process.env.npm_package_version,
     status: 'OK',
   });
+});
+
+// get localhost:5000
+app.get("/", (__, res) => {
+  res.json({ message: "Backend APi-NOSQL pfsairqualityg8 running " });
 });
 
 server.listen(PORT, (error) => {
@@ -21,7 +37,7 @@ server.listen(PORT, (error) => {
           --------------------------------
           --------------------------------
 
-          API branch cvasquez:
+          API:
 
           Status: Error
           Log: ${error}
@@ -36,7 +52,7 @@ server.listen(PORT, (error) => {
           --------------------------------
           --------------------------------
 
-          API branch cvasquez:
+          API:
 
           Name: Express API
           Port: ${PORT}
