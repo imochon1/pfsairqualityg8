589d9041b6a0502d2e478bf6a8925565119bfa34
carlos vasquez <carlosavrelsalvador@gmail.com>
2022-02-15 22:58:28 -0600

    fix mongoose

--- src/api/config.js
+++ src/api/config.js
@@ -2,13 +2,13 @@ const { NODE_ENV, ATLAS_USER, ATLAS_PASSWORD } = process.env;
 
 const databases = {
   dev: {
-    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.xjjjt.mongodb.net/pfsairqualityg8-dev?retryWrites=true&w=majority`,
+    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.q53vc.mongodb.net/pfsairqualityg8-dev?retryWrites=true&w=majority`,
   },
   stage: {
-    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.xjjjt.mongodb.net/pfsairqualityg8-staging?retryWrites=true&w=majority`,
+    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.q53vc.mongodb.net/pfsairqualityg8-staging?retryWrites=true&w=majority`,
   },
   prod: {
-    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.xjjjt.mongodb.net/pfsairqualityg8?retryWrites=true&w=majority`,
+    mongo_uri: `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@cluster0.q53vc.mongodb.net/pfsairqualityg8?retryWrites=true&w=majority`,
   },
 };
 
