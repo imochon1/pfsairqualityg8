fa316b2c49d0374d1dbce6d370f94fce054a0dc6
carlos vasquez <carlosavrelsalvador@gmail.com>
2022-02-16 20:20:33 -0600

    fix route

--- src/api/controllers/PlantController.js
+++ src/api/controllers/PlantController.js
@@ -1,10 +1,13 @@
 const Plant = require("../models/Plant");
 const storage = require("../utils/storage");
+const config = require("../config");
 
 module.exports = {
   // key : value
   findAll: async (req, res) => {
     try {
+      console.log("mongo_uri", process.env.MONGO_URI);
+      console.log("mongo_uri", config.mongo_uri);
       const allActivePlants = await Plant.find({ is_active: true });
       res.status(200).json({ message: "All plants ", plants: allActivePlants });
     } catch (error) {
